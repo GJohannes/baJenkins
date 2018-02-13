a5cb9a9128bc327d518ccd3f6871a1aa04190246
Johannes <Johannes@Zephira>
2018-02-13 11:18:49 +0100

    random change

--- test4Jenkins/src/mainPackage/MainStartClass.java
+++ test4Jenkins/src/mainPackage/MainStartClass.java
@@ -2,7 +2,12 @@ package mainPackage;
 
 public class MainStartClass {
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World random java code");
+		for(int i = 0; i < 10 ; i++){
+			i = i + 1;
+			System.out.println(i + " :is the current run");
+			
+		}
 	}
 }
 
