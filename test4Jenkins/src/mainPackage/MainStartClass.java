f4f3a41e4b83daf296431b6acf92af41370703a8
GJohannes <johannes.guenthoer@t-online.de>
2018-04-04 16:11:51 +0200

    empty line

--- test4Jenkins/src/mainPackage/MainStartClass.java
+++ test4Jenkins/src/mainPackage/MainStartClass.java
@@ -1,7 +1,17 @@
 package mainPackage;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainStartClass {
 	public static void main(String[] args){
+		
+		List list = new  ArrayList<>();
+		System.out.println(getListOfAllFilesInAFolderAndSubFolder("F:\\WindowsApps", list));
+		
+		// empty line
+		
 		System.out.println("Hello World random java code");
 		for(int i = 0; i < 10 ; i++){
 			i = i + 1;
@@ -17,5 +27,24 @@ public class MainStartClass {
 		System.out.println("This string only apears if the last push was successful");
 		System.out.println("          ");
 	}
+	
+	private static List<File> getListOfAllFilesInAFolderAndSubFolder(String path, List<File> allFilesInAFolderAndSubFolder) {
+		File directory = new File(path);
+		File[] fileList = directory.listFiles();
+
+		if (fileList != null) {
+			for (int i = 0; i < fileList.length; i++) {
+				if (fileList[i].isFile()) {
+					allFilesInAFolderAndSubFolder.add(fileList[i]);
+				} else if (fileList[i].isDirectory()) {
+					getListOfAllFilesInAFolderAndSubFolder(fileList[i].getAbsolutePath(), allFilesInAFolderAndSubFolder);
+					// add of directory
+					allFilesInAFolderAndSubFolder.add(fileList[i]);
+				}
+			}
+		}
+		return allFilesInAFolderAndSubFolder;
+	}
+	
 }
 
